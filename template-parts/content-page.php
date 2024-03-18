3790ab09ba39b1f8fa348115f85463aa1c898315
Mattias Lager <mattias@addkolon.se>
2024-03-18 14:12:30 +0100

    Added more styling and prepared

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -14,7 +14,7 @@
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
 
-	<?php my_first_theme_post_thumbnail(); ?>
+	
 
 	<div class="entry-content">
 		<?php
